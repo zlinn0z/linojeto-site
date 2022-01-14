43f4ac38eae96b9ecdc9939190f2351e57ee035f
Lino <vflino@gmail.com>
2022-01-13 23:22:39 -0300

    Criação de controller de login

---
+++ Server/Linojeto/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using Linojeto.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Linojeto.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult EfetuarLogin([FromBody] LoginRequisicaoDto requisicao)
+        {
+            try
+            {
+                if(requisicao == null || requisicao.Login == null || requisicao.Senha == null)
+                {
+                    return BadRequest(new ErrorLogsDto()
+                    {
+                        Status = StatusCodes.Status400BadRequest,
+                        Error = "Parâmetros de entrada inválidos!"
+                    });
+                }
+
+                return Ok("Usuário autenticado com sucesso!");
+            }
+            catch(Exception e)
+            {
+                _logger.LogError("Ocorreu erro ao efeftuar login", e, requisicao);
+                return StatusCode(StatusCodes.Status500InternalServerError, new ErrorLogsDto() {
+                    Status = StatusCodes.Status500InternalServerError,
+                    Error = "Ocorreu erro ao efetuar login, tente novamente!"
+                });
+            }
+        }
+    }
+}
---
+++ Server/Linojeto/Dtos/ErrorLogsDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Linojeto.Dtos
+{
+    public class ErrorLogsDto
+    {
+        public int Status { get; set; }
+        public string Error { get; set; }
+    }
+}
